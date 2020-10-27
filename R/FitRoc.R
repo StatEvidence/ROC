2c24ea12283944d587e9c4a5ce4d795973ca4fae
StatEvidence <32695115+StatEvidence@users.noreply.github.com>
2020-10-27 17:56:27 -0500

    Update FitRoc.R

--- R/FitRoc.R
+++ R/FitRoc.R
@@ -57,7 +57,7 @@ fit.roc <- function(roc.obj, f.1=NULL, f.2=NULL, f.3=NULL, f.4=NULL,
 						model="binormal",   
 						dist.0=NA, dist.1=NA,
 						dpar.0=NA, dpar.1=NA, 
-						start.parms=NA, method="BFGS", ...) {
+						start.parms=NA, method="BFGS", maxit, ...) {
 							
 #### Errors / Warnings
 
@@ -152,7 +152,7 @@ fit.roc <- function(roc.obj, f.1=NULL, f.2=NULL, f.3=NULL, f.4=NULL,
 					model=model, 
 					dist.0=dist.0, dpar.0=dpar.0,
 					dist.1=dist.1, dpar.1=dpar.1, 
-					hessian=TRUE, method=method, ...)
+					hessian=TRUE, method=method, control=list(maxit=maxit), ...)
 	
 	fit.vcov 	<- solve(mlfit$hessian) 	# not negative because optim minimizing (-1)*log-like
 	fit.se 		<- sqrt(diag(fit.vcov))
